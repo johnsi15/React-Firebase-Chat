bbb8d2e9c47cf491b97a828b7d9af4cc1e5ba062
john andrey <andrey_201015@hotmail.com>
2017-11-12 17:37:58 -0500

    Agregando database con firebase y components para chat en real time probar que todo este bien

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import firebase from 'firebase';
 
 import Chat from './Chat.jsx';
 import Header from './Header.jsx';
+import ChatMessage from './ChatMessage.jsx';
 
 // class App extends Component { }
 class App extends React.Component {
@@ -18,6 +19,7 @@ class App extends React.Component {
     }
     this.handleAuth = this.handleAuth.bind(this);
     this.handleLogout = this.handleLogout.bind(this);
+    this.handleSendMessage = this.handleSendMessage.bind(this);
   }
 
   /* 
@@ -25,6 +27,14 @@ class App extends React.Component {
     Este método se ejecuta cuando el componente se está por renderizar. En este punto es posible modificar el estado del componente sin causar una actualización (y por lo tanto no renderizar dos veces el componente). Es importante sin embargo evitar causar cualquier clase de efecto secundario (petición HTTP por ejemplo) ya que este método se ejecuta en el servidor y hacer esto puede causar problemas de memoria.
   */
   componentWillMount () {
+    const messageDB = firebase.database().ref().child('messages');
+
+    messageDB.on('child_added', snap => {
+      this.setState({
+        messages: this.state.messages.concat(snap.val())
+      });
+    });
+
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({
@@ -51,6 +61,27 @@ class App extends React.Component {
       .then(() => console.log('Te has desconectado'))
       .catch(error => console.log(`Error: ${error.code}: ${error.message}`))
   }
+
+  handleSendMessage (text) {
+    const messages = firebase.database().ref().child('messages');
+
+    let newUserMessage = message.push();
+    let msg = {
+      text: text,
+      avatar: this.state.user.photoURL,
+      displayName: this.state.user.displayName
+    }
+    newUserMessage.set(msg);
+
+  }
+
+  renderMessages () {
+    if (this.state.user) {
+      return this.state.messages.map(msg => {
+        <ChatMessage message={msg} />
+      }).reverse()
+    }
+  }
   
   // Tener en cuenta el estado "state" no se debe modificar nunca en este punto del render.
   render () {
@@ -61,6 +92,13 @@ class App extends React.Component {
           user={this.state.user}
           onAuth={this.handleAuth}
           onLogout={this.handleLogout}
+        />
+        <div className='message-chat-list container'>
+          <br/><br/>
+          {this.renderMessages()}
+        </div>
+        <Chat
+          onSendMessage={this.handleSendMessage}
         />
       </section>
     )
